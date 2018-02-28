49000c9415d81676aa3dcb60f92d8ad7e1aa2d2d
LongYingying <32573265+LongYingying@users.noreply.github.com>
2018-02-28 20:45:43 +0800

    Add files via upload

---
+++ AddTwoNumbersClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+    class AddTwoNumbersClass
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode head=new ListNode(0) ;
+            ListNode currentNode = head;
+            int flag = 0;//判断两数相加是否大于等于10
+            int sum = 0;//计算两数相加之和
+            while (l1.next != null&& l2.next != null)
+            {
+                sum = l1.val + l2.val;
+                int x = sum % 10 + flag;
+
+
+                currentNode.next  = new ListNode(sum % 10 + flag); ;
+               
+                if (sum >= 10)
+                    flag = 1;
+                else
+                    flag = 0;
+                l1 = l1.next;
+                l2 = l2.next;
+                currentNode = currentNode.next;
+            }
+
+            while (l1.next != null)
+            {
+                currentNode = new ListNode(l1.val + flag);
+                flag = 0;
+                currentNode = currentNode.next;
+                l1 = l1.next;
+            }
+            while (l2.next != null)
+            {
+                currentNode = new ListNode(l2.val + flag);
+                flag = 0;
+                currentNode = currentNode.next;
+                l2 = l2.next;
+            }
+            return head.next;
+        }
+    }
+}
---
+++ HammingDistanceClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class HammingDistanceClass
+    {
+        public int HammingDistance(int x, int y)
+        {
+            int dis=0;
+            List<int> xx = new List<int>();
+            List<int> yy = new List<int>();
+            
+            while(x/2!=0||x%2!=0)
+            {
+                xx.Add(x%2);
+                x = x / 2;
+            }
+            while(y / 2 != 0 || y % 2 != 0)
+            {
+                yy.Add(y % 2);
+                y = y / 2;
+            }
+            int xCount = xx.Count;
+            int yCount = yy.Count;
+            int MinCount=xCount;
+            if (xCount > yCount)
+            {
+                while (xCount != yCount)
+                {
+                    yy.Add(0);
+                    yCount++;
+                }
+            }
+            else
+            {
+                while (xCount != yCount)
+                {
+                    xx.Add(0);
+                    xCount++;
+                }
+            }
+
+            
+            for(int i=0;i<xCount;i++)
+            {
+                if (xx[i] != yy[i])
+                    dis++;
+            }
+
+            return dis;
+        }
+    }
+}
